97e8d839d8f3805351a59b3fa7c8b3787ffd274b
Rashid Ashaf Prangon <72270233+prangonxD4375@users.noreply.github.com>
2026-06-24 22:49:25 +0200

    Add: string-encode-and-decode - submission-1

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-1.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+
+    string encode(vector<string>& strs) {
+        string encoded;
+
+        //"5#hello5#world"
+        for(const auto &s: strs){
+            encoded += to_string(s.size())+"#"+ s;
+        }
+        return encoded;
+    }
+
+    vector<string> decode(string s) {
+        vector <string> result;
+
+        int i=0;
+
+        while(i<s.size()){
+            int j=i;
+            while(s[j]!='#'){
+                j++;
+            }
+            int len=stoi(s.substr(i,j-i));
+            j++;
+            result.push_back(s.substr(j, len));
+            i=j+len;
+        }
+        return result;
+    }
+};
